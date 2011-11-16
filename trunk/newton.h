74f397b7ab2a12211617773adf7aa752436512e7
keithalewis_cp <SND\keithalewis_cp>
2011-11-16 06:28:14 -0800

    Newton tailored for dual numbers.

--- trunk/newton.h
+++ trunk/newton.h
@@ -8,17 +8,36 @@ namespace root {
 namespace _1d {
 
 	template<class T>
-	inline T newton(T x, T f, T df)
+	inline T newton(const T& x, const T& f, const T& df)
 	{
 		return x - f/df;
 	}
 
 	template<class T>
-	inline T newton(T x, T f, T df, T ddf)
+	inline T newton(const T& x, const T& f, T df, const T& ddf)
 	{
 		T a = ddf/2;
 		T b = df - x*ddf;
 		T c = f - x*df + x*x*ddf/2;
+
+		return same_sign(f, df) ? quadratic(a, b, c).second : quadratic(a, b, c).first;
+	}
+	template<class T>
+	inline T newton(const T& x, const T* f)
+	{
+		T a = f[2]/2;
+		T b = f[1] - x*f[2];
+		T c = f[0] - x*f[1] + x*x*f[2]/2;
+
+		return same_sign(f, df) ? quadratic(a, b, c).second : quadratic(a, b, c).first;
+	}
+	// for use with dual numbers
+	template<class T>
+	inline T newton_2(const T& x, const T* f)
+	{
+		T a = f[2]/2;
+		T b = f[1] - x*f[2];
+		T c = f[0] - x*f[1] + x*x*f[2];
 
 		return same_sign(f, df) ? quadratic(a, b, c).second : quadratic(a, b, c).first;
 	}
